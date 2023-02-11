ed412feed4b0cb3b1697fd5443ef004e8265a356
Vitor Hugo <custodio_vitor@outlook.com>
2023-02-11 19:08:40 -0300

    Added env vars and mongodb setup

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UniversityModule } from './university/university.module';
 
-@Module({ imports: [UniversityModule] })
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_DATABASE_CONNECTION_URI),
+    UniversityModule,
+  ],
+})
 export class AppModule {}
